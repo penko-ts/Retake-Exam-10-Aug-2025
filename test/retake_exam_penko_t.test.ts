c4ebf200cd06d00df363cc06dac689b253734b96
penko-ts <penko.aprilci@gmail.com>
2025-08-10 09:16:38 +0300

    Initial commit

---
+++ test/retake_exam_penko_t.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as RetakeExamPenkoT from '../lib/retake_exam_penko_t-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/retake_exam_penko_t-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new RetakeExamPenkoT.RetakeExamPenkoTStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
